f5af5da2dd41ae2ad1a8a1a622cca3f28b09254e
Jan Poonthong <janpoonthong628@gmail.com>
2021-02-07 16:04:53 +0700

    Add sound when the ball hit the window and increase speed of the ball

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ function fillCircle(context, x, y, radius, color="red") {
 
 (() => {
     const canvas = document.getElementById("game");
+    const sound = document.getElementById("sound");
     const radius = 69;
     const context = canvas.getContext("2d");
-    const speed = 100
+    const speed = 150
 
     let start;
     let x = radius + 10;
@@ -29,8 +30,18 @@ function fillCircle(context, x, y, radius, color="red") {
         canvas.width = width;
         canvas.height = height;
 
-        if (x + radius >= width  || x - radius <= 0) deltaX = -deltaX;
-        if (y + radius >= height || y - radius <= 0) deltaY = -deltaY;
+        if (x + radius >= width  || x - radius <= 0) {
+            deltaX = -deltaX;
+            sound.pause();
+            sound.currentTime = 0;
+            sound.play();
+        }
+        if (y + radius >= height || y - radius <= 0) {
+            deltaY = -deltaY;
+            sound.pause();
+            sound.currentTime = 0;
+            sound.play();
+        }
 
         x += deltaX * deltaT;
         y += deltaY * deltaT;
